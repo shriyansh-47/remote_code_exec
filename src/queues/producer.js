f9f2a8944b041c946f74eb7ab74e229709a29502
Shriyansh <shriyansh947@gmail.com>
2026-08-29 05:01:49 +0530

    job producer setup using BullMQ

---
+++ src/queues/producer.js
@@ -0,0 +1,32 @@
+import {Queue} from 'bullmq'
+import {redisConnection} from '../config/redis.js'
+import { asyncHandler } from '../utils/asyncHandler.js'
+
+const submissionQueue = new Queue('submission-queue' , {
+    connection : redisConnection
+})
+// names a section (BullMQ queue) inside the redis db (BullMQ uses redis data-struct to manage the queue)
+// where new jobs will be added and pending would be fetched from.
+
+const addJobToQueue = async (jobID, language, srcCode) =>{
+    // .add() returns metadata about the queue object like timestamp of addition,
+    // payload data, internal ID, etc.
+    const job = await submissionQueue.add("code-execution", {
+        jobID,
+        language,
+        srcCode
+    })
+    // code-execution is the name for the job
+    // it adds a json object in the queue like {job-name , job-data}
+    // where job-data = {jobID, language, srcCode}
+
+    console.log(`Job-${jobID} added to the queue`)
+    return job
+    
+}
+
+// no error handling since Error-Bubling will be used and the error
+// will be caught by asyncHandler in submitCode controller.
+export {addJobToQueue}
+
+
